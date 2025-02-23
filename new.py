267a2857071215df1f8c801cfe785785fe410a0d
Yuvraj Sharma <yuvrajsharma9109@gmail.com>
2025-02-23 11:51:39 +0530

    ..

--- new.py
+++ new.py
@@ -1,6 +1,6 @@
 import numpy as nd
 array=nd.array([1,3,2,1,3])
 print(array)
-print(type(array))
-print(nd.__version__)
-print("helloworld")
+arr2=nd.array([[[1,2,3],[1,2,77]],[[1,2,3],[1,2,90]]])
+print("the dimension is ",arr2.ndim)
+print(arr2[1:-3:-1])
